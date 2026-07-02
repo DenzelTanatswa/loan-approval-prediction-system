5400162e045a2d77c29084a878d10dd47f41b633
DenzelTanatswa <151496802+DenzelTanatswa@users.noreply.github.com>
2026-07-02 11:22:11 +0200

    too coolll

--- Approval App/AppBuild.py
+++ Approval App/AppBuild.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import pandas as pd
-import joblib 
-model = joblib.load("models/loan_approval_model.pkl") 
-encoder = joblib.load("models/label_encoder.pkl")
+import joblib
+model = joblib.load("C:\\Users\\denze\\OneDrive\\Desktop\\Project\\loan-approval-prediction-system\\loan-approval-prediction-system\\Models\\loan_approval_model.pkl")
+encoder = joblib.load("C:\\Users\\denze\\OneDrive\\Desktop\\Project\\loan-approval-prediction-system\\loan-approval-prediction-system\\Models\\label_encoder.pkl")
 st.title("🏦 Loan Approval Prediction System")
 
 st.write("Enter the applicant details below.")
+dependents = st.number_input("Number of Dependents", 
+                             min_value=0,
+                               max_value=10,
+                                 value=0
+                                 )
+
+education = st.selectbox("Education",
+                         ["Graduate", "Not Graduate"]
+)
